4abd73baf18a29fbff1f92bae87138c79f34de3b
bindhada <binne_@live.se>
2026-09-25 19:38:28 +0200

    Add GET transaction by ID

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { transactions } from "./data.js";
 
 const app = express();
 const PORT = 3000;
@@ -7,6 +8,26 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Internet Bank Transaction API");
+});
+
+app.get("/transactions/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    if (Number.isNaN(id)) {
+        return res.status(400).json({
+            error: "Transaction ID must be a number"
+        });
+    }
+
+    const transaction = transactions.find((transaction) => transaction.id === id);
+
+    if (!transaction) {
+        return res.status(404).json({
+            error: "Transaction not found"
+        });
+    }
+
+    return res.status(200).json(transaction);
 });
 
 app.listen(PORT, () => {
