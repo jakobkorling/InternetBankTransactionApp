fe30dc8f8b38e308942cbae4a9cf6f08d17e2153
Zhuoxuan C <zhuoxuan.chen00@gmail.com>
2026-09-25 17:47:07 +0200

    Add date filtering to GET /transactions

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { transactions, findClassification } from "./data.js";
+import { number } from "@inquirer/prompts";
 
 const app = express();
 const PORT = 3000;
@@ -11,6 +12,8 @@ app.get("/", (req, res) => {
 });
 
 
+// Link transcations with classifications
+
 app.get("/transactions", (req, res): void => {
     const result = transactions.map((t) => {
         if (t.amount < 0) {
@@ -20,6 +23,8 @@ app.get("/transactions", (req, res): void => {
     });
     res.status(200).json(result);
 });
+
+// Update transcations by id
 
 app.put("/transactions/:id", (req, res): void => {
     const transactionId: number = parseInt(req.params.id);
@@ -38,7 +43,7 @@ app.put("/transactions/:id", (req, res): void => {
     transaction.date = req.body.date || transaction.date;
     transaction.recipient = req.body.recipient || transaction.recipient;
     transaction.amount = req.body.amount ?? transaction.amount;     
-
+    
     if (transaction.amount < 0) {
         transaction.classification = findClassification(transaction.recipient);
     } else {
@@ -46,6 +51,41 @@ app.put("/transactions/:id", (req, res): void => {
     }
 
     res.status(200).json({ message:"Transaction updated successfully", transaction});
+});
+
+// Filter transcations by date
+app.get("/transactions", (req, res): void => {
+    const { start, end } = req.query;
+    let result = transactions;
+    if (start || end) {
+        const startDate = new Date(start as string);
+        const endDate = new Date(end as string);
+
+        // Handle invalid dates
+        if (isNaN(startDate.getTime()) || isNaN(endDate.getTime())) {
+            res.status(400).json({ message: "Invalid date format" });
+            return;
+        }
+        // Handle cases where the start date is after the end date
+        if (startDate > endDate) {
+            res.status(400).json({ message: "Start date must be before end date!"});
+            return;
+        } 
+        // Filter transcations by date
+        result = transactions.filter((t) => {
+            const transactionDate = new Date(t.date);
+            return transactionDate >= startDate && transactionDate <= endDate;
+        });
+    }
+    
+    const withClassification = result.map((t) => {
+        if (t.amount < 0) {
+            return { ...t, classification: findClassification(t.recipient) };
+        }
+        return t;
+    });
+
+    res.status(200).json(withClassification);
 });
 
 app.listen(PORT, () => {
